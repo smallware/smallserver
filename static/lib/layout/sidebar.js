8dfff0fdd362b45b513a8b77077a7bbd30874770
Andreas Abilio <andreas@abilio.dk>
2016-07-29 18:02:33 +0200

    icons

--- static/lib/layout/sidebar.js
+++ static/lib/layout/sidebar.js
@@ -15,10 +15,14 @@ import {List, ListItem} from 'material-ui/List';
 import ActionGrade      from 'material-ui/svg-icons/action/grade';
 import ContentInbox     from 'material-ui/svg-icons/content/inbox';
 import ContentDrafts    from 'material-ui/svg-icons/content/drafts';
-import DeveloperBoard   from 'material-ui/svg-icons/hardware/developer-board';
+import Storage          from 'material-ui/svg-icons/device/storage.js';
+import DeviceHub        from 'material-ui/svg-icons/hardware/device-hub.js';
+import VerifiedUser     from 'material-ui/svg-icons/action/verified-user';
 import Assessment       from 'material-ui/svg-icons/action/assessment.js';
 import Subscriptions    from 'material-ui/svg-icons/av/subscriptions.js';
+import ArtTrack         from 'material-ui/svg-icons/av/art-track.js';
 import Extension        from 'material-ui/svg-icons/action/extension.js';
+import Archive          from 'material-ui/svg-icons/content/archive.js';
 import People           from 'material-ui/svg-icons/social/people';
 import Subheader        from 'material-ui/Subheader';
 
@@ -42,72 +46,28 @@ const Sidebar = () => (
     <List className="body"
       style={{background: '#fff'}}>
       <ListItem primaryText="Dashboard" leftIcon={<Assessment />} />
+      <ListItem primaryText="Resources" leftIcon={<Subscriptions />} />
+      <ListItem primaryText="Services" leftIcon={<Extension />} />
       <ListItem
-        primaryText="Resources"
-        leftIcon={<Subscriptions />}
-        initiallyOpen={true}
+        primaryText="Security"
+        leftIcon={<VerifiedUser />}
+        initiallyOpen={false}
         primaryTogglesNestedList={true}
         nestedItems={[
           <ListItem
             key={1}
-            primaryText="Groups"
+            primaryText="Users"
             leftIcon={<ActionGrade />}
           />,
           <ListItem
             key={2}
-            primaryText="Sent Mail"
-            leftIcon={<DeveloperBoard />}
-            disabled={true}
-            nestedItems={[
-              <ListItem key={1} primaryText="Drafts" leftIcon={<ContentDrafts />} />,
-            ]}
-          />,
-        ]}
-      />
-      <ListItem
-        primaryText="Services"
-        leftIcon={<Extension />}
-        initiallyOpen={true}
-        primaryTogglesNestedList={true}
-        nestedItems={[
-          <ListItem
-            key={1}
             primaryText="Groups"
-            leftIcon={<ActionGrade />}
-          />,
-          <ListItem
-            key={2}
-            primaryText="Sent Mail"
-            leftIcon={<DeveloperBoard />}
-            disabled={true}
-            nestedItems={[
-              <ListItem key={1} primaryText="Drafts" leftIcon={<ContentDrafts />} />,
-            ]}
+            leftIcon={<People />}
           />,
         ]}
       />
-      <ListItem
-        primaryText="Users"
-        leftIcon={<People />}
-        initiallyOpen={true}
-        primaryTogglesNestedList={true}
-        nestedItems={[
-          <ListItem
-            key={1}
-            primaryText="Groups"
-            leftIcon={<ActionGrade />}
-          />,
-          <ListItem
-            key={2}
-            primaryText="Sent Mail"
-            leftIcon={<DeveloperBoard />}
-            disabled={true}
-            nestedItems={[
-              <ListItem key={1} primaryText="Drafts" leftIcon={<ContentDrafts />} />,
-            ]}
-          />,
-        ]}
-      />
+      <ListItem primaryText="Network" leftIcon={<DeviceHub />} />
+      <ListItem primaryText="Database" leftIcon={<Storage />} />
     </List>
   </div>
 );
